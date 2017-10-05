c20efa305519d052b2039e33fab8408846843485
Pierre Jacquet <pjacquet.enseirb@gmail.com>
2017-10-05 13:58:37 +0200

    MaJ exo1 ajout consigne.pdf

--- exo1.f90
+++ exo1.f90
@@ -1,12 +1,12 @@
-program exo1
-  use mpi
-  implicit none
-  integer :: code
+program exo1      !! Nom du code
+  use mpi         !! Package à ajouter pour utiliser OPEN-MPI
+  implicit none   !! Classique implicit none
+  integer :: code !! Déclaration d'un entier ici appelé code qui permettra l'initialisation du la zone parallèle du programme
 
-  call MPI_INIT(code)
+  call MPI_INIT(code)     !! Début de la zone parallèle
 
   print*,'Bonjour'
 
-  call MPI_FINALIZE(code)
-
-end program exo1
+  call MPI_FINALIZE(code) !! Fin de la zone parallèle
+  
+end program exo1  !! Fin du programme
